a8d0a8b7191ff6725635481efde52444295712f4
Avnish <84154241+avnish01000@users.noreply.github.com>
2022-12-28 19:54:50 +0530

    Update AppiumUtils.java
    Removed import org.apache.commons.*;

--- src/main/java/org/rahulshettyacademy/utils/AppiumUtils.java
+++ src/main/java/org/rahulshettyacademy/utils/AppiumUtils.java
@@ -11,7 +11,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.apache.commons.io.FileUtils;
-import org.apache.commons.*;
 
 import java.io.File;
 import java.io.IOException;
